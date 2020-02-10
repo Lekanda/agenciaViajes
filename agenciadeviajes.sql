1d8a0c221e220c17c9e6823c91c7cbde7a54e94b
lekanda <abernaolao@gmail.com>
2020-02-10 22:04:37 +0100

    Commit para Josue3

--- agenciadeviajes.sql
+++ agenciadeviajes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 10-02-2020 a las 20:52:13
+-- Tiempo de generación: 10-02-2020 a las 21:04:05
 -- Versión del servidor: 5.7.24
 -- Versión de PHP: 7.2.14
 
