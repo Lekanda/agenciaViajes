ba0076ab3060479065c0fe542bedc7d1b4c0e893
lekanda <abernaolao@gmail.com>
2020-02-10 21:17:50 +0100

    Commit para Josue

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -2,36 +2,36 @@ const Viaje = require('../models/Viajes');
 const Testimonial = require('../models/Testimoniales');
 
 
-exports.consultasHomepage = (req, res) => {
-    const promises = [];
-    promises.push(Viaje.findAll({
-         limit: 3 
-    }))
-    promises.push(Testimonial.findAll({
-         limit: 3
-    }))
+// exports.consultasHomepage = (req, res) => {
+//     const promises = [];
+//     promises.push(Viaje.findAll({
+//          limit: 3 
+//     }))
+//     promises.push(Testimonial.findAll({
+//          limit: 3
+//     }))
 
-    const resultado = Promise.all(promises);
+//     const resultado = Promise.all(promises);
 
-    resultado.then(resultado => res.render('index', {
-        pagina: 'Proximos Viajes',
-        clase: 'home',
-        viajes: resultado[0],
-        testimoniales: resultado[1]
-    }))
-    .catch(error => console.log((error)))
-}
-
-// exports.consultasHomepage = async (req, res) => {
-    
-//     const viajes = await Viaje.findAll({ limit: 3 });
-//     const testimoniales = await Testimonial.findAll({ limit: 3 });
-
-//     res.render('index', {
+//     resultado.then(resultado => res.render('index', {
 //         pagina: 'Proximos Viajes',
 //         clase: 'home',
-//         viajes,
-//         testimoniales
-//     })
-
+//         viajes: resultado[0],
+//         testimoniales: resultado[1]
+//     }))
+//     .catch(error => console.log((error)))
 // }
+
+exports.consultasHomepage = async (req, res) => {
+    
+    const viajes = await Viaje.findAll({ limit: 3 });
+    const testimoniales = await Testimonial.findAll({ limit: 3 });
+
+    res.render('index', {
+        pagina: 'Proximos Viajes',
+        clase: 'home',
+        viajes,
+        testimoniales
+    })
+
+}
--- server/controllers/testimonialesController.js
+++ server/controllers/testimonialesController.js
@@ -1,62 +1,15 @@
 const Testimonial = require('../models/Testimoniales');
 
-// exports.mostrarTestimoniales = async (req, res) => {
+exports.mostrarTestimoniales = async (req, res) => {
     
-//         const testimoniales = await Testimonial.findAll()
-//             res.render('testimoniales', {
-//                 pagina: 'Opiniones',
-//                 testimoniales
-//         })
-// }
-
-// exports.añadirTestimonial = async (req, res) => {
-//     //Validar que todos los campos esten llenos
-//     let {nombre, correo, mensaje} = req.body;
-//     let errores = [];
-//     if(!nombre) {
-//         errores.push({'mensaje' : 'Agrega tu Nombre'})            
-//     }
-//     if(!correo) {
-//         errores.push({'mensaje' : 'Agrega tu Correo'})            
-//     }
-//     if(!mensaje) {
-//         errores.push({'mensaje' : 'Agrega tu Mensaje'})            
-//     }
-    
-//     // Revisar por errores
-//     if(errores.length > 0 ) {
-//         //Muestra la vista con errores
-//         Testimonial.findAll()
-//          const testimoniales = await Testimonial.findAll()
-//          res.render('testimoniales', {
-//             errores,
-//             nombre,
-//             correo,
-//             mensaje,
-//             pagina:'Opiniones',
-//             testimoniales
-//         })
-//         // Almacena en la DB
-//         await Testimonial.create({
-//             nombre,
-//             correo,
-//             mensaje
-//         })
-//         res.redirect('testimoniales')
-//     }
-// }
-
-
-
-exports.mostrarTestimoniales = (req, res) => {
-    Testimonial.findAll()
-        .then(testimoniales => res.render('testimoniales', {
-            pagina: 'Opiniones',
-            testimoniales
-        }))
+        const testimoniales = await Testimonial.findAll()
+            res.render('testimoniales', {
+                pagina: 'Opiniones',
+                testimoniales
+        })
 }
 
-exports.añadirTestimonial = (req, res) => {
+exports.añadirTestimonial = async (req, res) => {
     //Validar que todos los campos esten llenos
     let {nombre, correo, mensaje} = req.body;
     let errores = [];
@@ -74,24 +27,71 @@ exports.añadirTestimonial = (req, res) => {
     if(errores.length > 0 ) {
         //Muestra la vista con errores
         Testimonial.findAll()
-         .then(testimoniales => res.render('testimoniales', {
-             errores,
-             nombre,
-             correo,
-             mensaje,
-             pagina:'Opiniones',
-             testimoniales
-         })
-         .catch(error => console.log(error))
-         )
-    }else {
+         const testimoniales = await Testimonial.findAll()
+         res.render('testimoniales', {
+            errores,
+            nombre,
+            correo,
+            mensaje,
+            pagina:'Opiniones',
+            testimoniales
+        })
         // Almacena en la DB
-        Testimonial.create({
+        await Testimonial.create({
             nombre,
             correo,
             mensaje
         })
-        .then(testimonial => res.redirect('testimoniales'))
-        .catch(error => console.log(error))
+        res.redirect('testimoniales')
     }
 }
+
+
+
+// exports.mostrarTestimoniales = (req, res) => {
+//     Testimonial.findAll()
+//         .then(testimoniales => res.render('testimoniales', {
+//             pagina: 'Opiniones',
+//             testimoniales
+//         }))
+// }
+
+// exports.añadirTestimonial = (req, res) => {
+//     //Validar que todos los campos esten llenos
+//     let {nombre, correo, mensaje} = req.body;
+//     let errores = [];
+//     if(!nombre) {
+//         errores.push({'mensaje' : 'Agrega tu Nombre'})            
+//     }
+//     if(!correo) {
+//         errores.push({'mensaje' : 'Agrega tu Correo'})            
+//     }
+//     if(!mensaje) {
+//         errores.push({'mensaje' : 'Agrega tu Mensaje'})            
+//     }
+    
+//     // Revisar por errores
+//     if(errores.length > 0 ) {
+//         //Muestra la vista con errores
+//         Testimonial.findAll()
+//          .then(testimoniales => res.render('testimoniales', {
+//              errores,
+//              nombre,
+//              correo,
+//              mensaje,
+//              pagina:'Opiniones',
+//              testimoniales
+//          })
+//          .catch(error => console.log(error))
+//          )
+//     }else {
+//         // Almacena en la DB
+//         Testimonial.create({
+//             nombre,
+//             correo,
+//             mensaje
+//         })
+//         .then(testimonial => res.redirect('testimoniales'))
+//         .catch(error => console.log(error))
+//     }
+// }
