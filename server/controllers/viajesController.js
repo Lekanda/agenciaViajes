5d38b07cbaf486c81d14554b4b28d5abda65b246
lekanda <abernaolao@gmail.com>
2020-02-10 20:48:45 +0100

    prueba PROMESAS ANTIGUAS 6

--- server/controllers/viajesController.js
+++ server/controllers/viajesController.js
@@ -1,18 +1,36 @@
 const Viaje = require('../models/Viajes');
-
-exports.mostrarViajes = (req, res) => {
+exports.mostrarViajes =  (req, res) => {
     Viaje.findAll()
         .then(viajes => res.render('viajes', {
             pagina: 'Proximos Viajes',
             viajes
-        } ))
+        }))
         .catch(error => console.log(error))
-}
-
-exports.mostrarViaje = (req, res) => {
-    Viaje.findByPk(req.params.id)
-        .then (viaje = res.render('viaje', {
+    }
+    
+    exports.mostrarViaje = (req, res) => {
+        Viaje.findByPk(req.params.id)
+        .then(viaje => res.render('viaje', {
+            pagina: 'Proximo Viaje',
             viaje
         }))
         .catch(error => console.log(error));
 }
+
+
+
+
+// exports.mostrarViajes = async (req, res) => {
+//     const viajes = await Viaje.findAll()
+//     res.render('viajes', {
+//             pagina: 'Proximos Viajes',
+//             viajes
+//         });
+// }
+
+// exports.mostrarViaje = async (req, res) => {
+//     const viaje = await Viaje.findByPk(req.params.id)
+//     res.render('viaje', {
+//             viaje
+//         })
+// }
